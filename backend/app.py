1656281c1e38b966b7591e85d4c850f2d91758dd
AmanPr33tS1ngh <amanpr333tsingh01@gmail.com>
2024-04-23 23:18:42 +0530

    added models

---
+++ backend/app.py
@@ -0,0 +1,7 @@
+from typing import List
+import fastapi as _fastapi
+import fastapi.security
+
+import sqlalchemy.orm
+
+app = _fastapi.FastAPI()
--- backend/models.py
+++ backend/models.py
@@ -9,6 +9,8 @@ import database
 class User(database.Base):
     __tablename__ = "users"
     id = sql.Column(sql.Integer, primary_key=True, index=True)
+    name = sql.Column(sql.String, index=True)
+    username = sql.Column(sql.String, unique=True, index=True)
     email = sql.Column(sql.String, unique=True, index=True)
     hashed_password = sql.Column(sql.String)
     
@@ -16,3 +18,24 @@ class User(database.Base):
         return hash.bcrypt.verify(password, self.hashed_password)
 
     
+class Product(database.Base):
+    __tablename__ = "products"
+    id = sql.Column(sql.Integer, primary_key=True, index=True)
+    price = sql.Column(sql.Integer, default=0)
+    name = sql.Column(sql.String(100), unique=True, index=True)
+    url = sql.Column(sql.String(100), unique=True, index=True)
+    image = sql.Column(sql.String,)
+    category = sql.Column(sql.String(25))
+    description = sql.Column(sql.String(255))
+    
+    
+class Order(database.Base):
+    __tablename__ = "orders"
+    id = sql.Column(sql.Integer, primary_key=True, index=True)
+    quantity = sql.Column(sql.Integer, nullable=False)
+    user_id = sql.Column(sql.Integer, sql.ForeignKey("users.id"))
+    product_id = sql.Column(sql.Integer, sql.ForeignKey("products.id"))
+    order_amount = sql.Column(sql.Integer, nullable=False, )
+    transaction_id = sql.Column(sql.String, unique=True)
+    is_delivered = sql.Column(sql.Boolean, default=False)
+    created_at = sql.Column(sql.DateTime, default=datetime.utcnow)
