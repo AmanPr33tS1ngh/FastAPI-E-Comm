7aad12c0a586a49f364205ae124229698e4ccbc0
AmanPr33tS1ngh <amanpr333tsingh01@gmail.com>
2024-05-01 17:01:43 +0530

    removed __pycache__ and create_db fix

--- backend/create_db.py
+++ backend/create_db.py
@@ -6,5 +6,5 @@ def create_database():
     Base.metadata.create_all(bind=engine)
     print("Database tables created successfully!")
 
-# if __name__ == '__main__':
-#     create_database()
+if __name__ == '__main__':
+    create_database()
